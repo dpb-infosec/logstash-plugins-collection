f91e85fa1e2cdb3e03ce3488dd5825a0bc978f1d
yannickwellens <yannick.wellens@delen.be>
2025-02-13 15:27:21 +0100

    fix for no hostname

--- logstash-codec-syslog/lib/logstash/codecs/syslog/parser.rb
+++ logstash-codec-syslog/lib/logstash/codecs/syslog/parser.rb
@@ -68,7 +68,7 @@ class LogStash::Codecs::Syslog::Parser
   # -------------------------------------------------------
   RFC5424_REGEX = /^(?:<(?<pri>\d+)>|)(?<version>\d+)\s+(?<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2}))\s+(?<hostname>\S+)\s+(?<app_name>[^:\s]+)(?::)?\s+(?<procid>\S+)\s+(?<msgid>\S+)\s+(?<structured_data>(?:\[[^\]]*\])+|-)\s*(?<message>.*)?$/x.freeze
 
-  RFC3164_REGEX = /^(?:<(?<pri>\d+)>|)(?<timestamp>(?:[A-Za-z]{3}\s+\d+\s+\d{1,2}:\d{2}:\d{2})|(?:\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:[+-]\d{2}:\d{2}|Z)))\s+(?<hostname>\S+)\s+(?<message>.*)$/x.freeze
+  RFC3164_REGEX = /^(?:<(?<pri>\d+)>)(?<timestamp>(?:[A-Za-z]{3}\s+\d+\s+\d{1,2}:\d{2}:\d{2})|(?:\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:[+-]\d{2}:\d{2}|Z)))\s+(?:(?<hostname>\S+)\s+)?(?<source>\S+):\s+(?<message>.*)$/x.freeze
   
   RFCUNIX_REGEX = /^(?:<(?<pri>\d+)>|)(?<timestamp>[A-Z][a-z]{2}\s+\d{2}:\d{2}:\d{2})\s+Message\sforwarded\sfrom\s(?<hostname>\S+):\s+(?<app_name>\w+)(?:\[(?<procid>\d+)\])?:\s+(?<message>.*)$/x.freeze
   
